6cde734d0452d6b8c64808ac91eb9449f1422888
j-rockwell <jrobertrockwell@gmail.com>
2020-04-15 00:15:22 -0700

    Added saving all handling for Ares Accounts

--- src/main/java/com/llewkcor/ares/core/player/PlayerHandler.java
+++ src/main/java/com/llewkcor/ares/core/player/PlayerHandler.java
@@ -1,6 +1,9 @@
 package com.llewkcor.ares.core.player;
 
+import com.llewkcor.ares.commons.logger.Logger;
 import com.llewkcor.ares.commons.promise.SimplePromise;
+import com.llewkcor.ares.commons.util.bukkit.Scheduler;
+import com.llewkcor.ares.core.player.data.account.AccountDAO;
 import com.llewkcor.ares.core.player.data.account.AresAccount;
 import com.llewkcor.ares.core.player.menu.SettingsMenu;
 import lombok.AllArgsConstructor;
@@ -11,6 +14,26 @@ import org.bukkit.entity.Player;
 public final class PlayerHandler {
     @Getter public final PlayerManager manager;
 
+    public void saveAll(boolean blocking) {
+        if (blocking) {
+            Logger.warn("Preparing to save " + manager.getAccountRepository().size() + " Accounts");
+            manager.getAccountRepository().forEach(account -> AccountDAO.saveAccount(manager.getPlugin().getDatabaseInstance(), account));
+            Logger.print("Finished saving " + manager.getAccountRepository().size() + " Accounts");
+            return;
+        }
+
+        Logger.warn("Preparing to save " + manager.getAccountRepository().size() + " Accounts");
+        new Scheduler(manager.getPlugin()).async(() -> {
+            manager.getAccountRepository().forEach(account -> AccountDAO.saveAccount(manager.getPlugin().getDatabaseInstance(), account));
+            new Scheduler(manager.getPlugin()).sync(() -> Logger.print("Finished saving " + manager.getAccountRepository().size() + " Accounts")).run();
+        }).run();
+    }
+
+    /**
+     * Handles opening the settings menu
+     * @param player Player
+     * @param promise Promise
+     */
     public void openSettings(Player player, SimplePromise promise) {
         final AresAccount account = manager.getAccountByBukkitID(player.getUniqueId());
 
