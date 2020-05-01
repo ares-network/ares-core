3d271f4878d45e2a97d7b847e3f0fda5969a79ce
j-rockwell <jrobertrockwell@gmail.com>
2020-05-01 15:41:26 -0700

    Implemented Restart display & swapped rendering timer to sync thread

--- src/main/java/com/playares/core/timers/TimerManager.java
+++ src/main/java/com/playares/core/timers/TimerManager.java
@@ -12,6 +12,7 @@ import com.playares.core.timers.data.PlayerTimer;
 import com.playares.core.timers.data.type.PlayerTimerType;
 import com.playares.core.timers.event.HUDUpdateEvent;
 import com.playares.core.timers.listener.TimerListener;
+import com.playares.essentials.EssentialsService;
 import lombok.Getter;
 import net.minecraft.server.v1_8_R3.EntityVillager;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
@@ -33,36 +34,40 @@ public final class TimerManager {
     public TimerManager(Ares plugin) {
         this.plugin = plugin;
         this.handler = new TimerHandler(this);
-        this.timerTask = new Scheduler(plugin).async(() -> {
+        this.timerTask = new Scheduler(plugin).sync(() -> {
+            final EssentialsService essentialsService = (EssentialsService)plugin.getService(EssentialsService.class);
+
             for (AresPlayer profile : plugin.getPlayerManager().getPlayerRepository()) {
                 final Set<PlayerTimer> expired = profile.getTimers().stream().filter(Timer::isExpired).collect(Collectors.toSet());
                 final Set<PlayerTimer> timers = profile.getTimers().stream().filter(timer -> !timer.isExpired()).collect(Collectors.toSet());
 
                 expired.forEach(timer -> new Scheduler(plugin).sync(() -> handler.finishTimer(timer)).run());
 
-                new Scheduler(plugin).sync(() -> {
-                    final Player player = Bukkit.getPlayer(profile.getUniqueId());
-                    final HUDUpdateEvent hudUpdateEvent = new HUDUpdateEvent(player);
+                final Player player = Bukkit.getPlayer(profile.getUniqueId());
+                final HUDUpdateEvent hudUpdateEvent = new HUDUpdateEvent(player);
 
-                    if (player != null && !player.isDead()) {
-                        if (!timers.isEmpty()) {
-                            timers.stream().filter(activeTimer -> activeTimer.getType().isRender()).forEach(renderedTimer -> {
-                                if (renderedTimer.getType().isDecimal()) {
-                                    hudUpdateEvent.add(renderedTimer.getType().getDisplayName() + " " + ChatColor.RED + Time.convertToDecimal(renderedTimer.getExpire() - Time.now()) + "s");
-                                } else {
-                                    hudUpdateEvent.add(renderedTimer.getType().getDisplayName() + " " + ChatColor.RED + Time.convertToHHMMSS(renderedTimer.getExpire() - Time.now()));
-                                }
-                            });
-                        }
-
-                        Bukkit.getPluginManager().callEvent(hudUpdateEvent);
-
-                        final String hud = Joiner.on(ChatColor.RESET + " " + ChatColor.RESET + " ").join(hudUpdateEvent.getEntries());
-                        final PacketPlayOutChat packet = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + hud + "\"}"), (byte) 2);
-
-                        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+                if (player != null && !player.isDead()) {
+                    if (essentialsService != null && essentialsService.getRebootManager().isRebootInProgress()) {
+                        hudUpdateEvent.add(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Restart" + ChatColor.RESET + " " + ChatColor.RED + Time.convertToHHMMSS(essentialsService.getRebootManager().getTimeUntilReboot()));
                     }
-                }).run();
+
+                    if (!timers.isEmpty()) {
+                        timers.stream().filter(activeTimer -> activeTimer.getType().isRender()).forEach(renderedTimer -> {
+                            if (renderedTimer.getType().isDecimal()) {
+                                hudUpdateEvent.add(renderedTimer.getType().getDisplayName() + " " + ChatColor.RED + Time.convertToDecimal(renderedTimer.getExpire() - Time.now()) + "s");
+                            } else {
+                                hudUpdateEvent.add(renderedTimer.getType().getDisplayName() + " " + ChatColor.RED + Time.convertToHHMMSS(renderedTimer.getExpire() - Time.now()));
+                            }
+                        });
+                    }
+
+                    Bukkit.getPluginManager().callEvent(hudUpdateEvent);
+
+                    final String hud = Joiner.on(ChatColor.RESET + " " + ChatColor.RESET + " ").join(hudUpdateEvent.getEntries());
+                    final PacketPlayOutChat packet = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + hud + "\"}"), (byte) 2);
+
+                    ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+                }
             }
         }).repeat(0L, 1L).run();
 
