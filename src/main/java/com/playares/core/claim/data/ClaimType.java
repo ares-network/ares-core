0a7d62dd507a432c7fbf7767adbbaa517e58e7d4
j-rockwell <jrobertrockwell@gmail.com>
2020-05-02 02:07:52 -0700

    Rebalanced claim materials, added Emerald reinforcements

--- src/main/java/com/playares/core/claim/data/ClaimType.java
+++ src/main/java/com/playares/core/claim/data/ClaimType.java
@@ -7,8 +7,9 @@ import org.bukkit.Material;
 @AllArgsConstructor
 public enum ClaimType {
     STONE("Stone", 100, 600, Material.STONE),
-    IRON("Iron", 600, 1800, Material.IRON_INGOT),
-    DIAMOND("Diamond", 1200, 10800, Material.DIAMOND);
+    IRON("Iron", 300, 1800, Material.IRON_INGOT),
+    DIAMOND("Diamond", 600, 10800, Material.DIAMOND),
+    EMERALD("Emerald", 1200, 21600, Material.EMERALD);
 
     @Getter public final String displayName;
     @Getter public final int durability;
