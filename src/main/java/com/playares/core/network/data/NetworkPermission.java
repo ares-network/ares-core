1365c743c50721f03de27ea1f78649f66d413893
j-rockwell <jrobertrockwell@gmail.com>
2020-05-08 12:41:37 -0700

    Updated default network member permissions

--- src/main/java/com/playares/core/network/data/NetworkPermission.java
+++ src/main/java/com/playares/core/network/data/NetworkPermission.java
@@ -9,12 +9,12 @@ public enum NetworkPermission {
     ACCESS_CHAT("Access Chat", "Grants the ability to receive/send network messages", true),
     KICK_MEMBERS("Kick Members", "Grants the ability to kick players who aren't Admins", false),
     INVITE_MEMBERS("Invite Members", "Grants the ability to invite players", false),
-    MODIFY_CLAIMS("Claim/Unclaim Land", "Grants the ability to claim and unclaim land", false),
+    MODIFY_CLAIMS("Claim/Unclaim Land", "Grants the ability to claim and unclaim land", true),
     ACCESS_LAND("Access Claims", "Grants the ability to access claims", true),
     VIEW_SNITCHES("View Snitch Notifications", "Grants access to all snitch notifications", true),
-    MODIFY_SNITCHES("Create Snitches", "Grants the ability to create snitches", false),
-    ACCESS_FACTORY("Access Factories", "Grants access to all factories", false),
-    MODIFY_FACTORY("Create Factories", "Grants the ability to create factories", false),
+    MODIFY_SNITCHES("Create Snitches", "Grants the ability to create snitches", true),
+    ACCESS_FACTORY("Access Factories", "Grants access to all factories", true),
+    MODIFY_FACTORY("Create Factories", "Grants the ability to create factories", true),
     MODIFY_BASTION("Create Bastions", "Grants the ability to create bastions", false),
     MODIFY_ACID("Create Acid", "Grants the ability to create acid blocks", false);
 
